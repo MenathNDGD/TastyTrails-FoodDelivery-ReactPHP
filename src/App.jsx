36244fe5c33b74e818ebe01e9f2ae927304505a4
Praveen Dilshan <rosapraveen1@gmail.com>
2024-07-09 22:33:39 +0530

    Modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer'
+
+
 
 const App = () => {
   return (
+    <>
     <div className='app'>
-      <Navbar />
+      <Navbar/>
+      <Footer/>
+
     </div>
+    </>
   )
 }
 
