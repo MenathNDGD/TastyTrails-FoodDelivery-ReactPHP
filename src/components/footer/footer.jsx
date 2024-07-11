03c6790cec9fa2e1909495d8d09cf9b605d16098
Menath Nadungoda <nadungmenath1@gmail.com>
2024-07-11 13:07:20 +0530

    modified with new footer content

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
             <div className="footer-content">
                 <div className="footer-content-left">
                     <img src={assets.logo} alt="" className="footer-logo"/>
-                    <p>dshfgdshf dshgbsdhgdsg sdhjbgdsghd ghdsgbsdhg sdgsghsdg sdbg sdhgbsdhgsd gsghsbgsdg sdghsdbgsd gsdhgsg sdhgbsdgs dgsdhg sg sgshg sdhgbsdg sdgsdbgsdgbsdjkgdsbg dsgdsb</p>
+                    <p>Discover a world of culinary delights delivered straight to your door. TastyTrails brings you the freshest, most delectable dishes from your favorite local eateries. From comfort food to gourmet meals, our curated selection ensures every bite is a new adventure. Join us on a journey of taste and enjoy hassle-free delivery, exceptional service, and the joy of dining in the comfort of your home.</p>
                     <div className="footer-social-icons">
                         <img src={assets.facebook_icon} alt="" />
                         <img src={assets.twitter_icon} alt="" />
@@ -32,13 +32,13 @@ const Footer = () => {
                     <h2>GET IN TOUCH</h2>
                     <ul>
                         <li>0750104549</li>
-                        <li>rosapraveen1@gamil.com</li>
+                        <li>tasty.info.trails@gamil.com</li>
                     </ul>
                 </div>
             </div>
             <hr/>
             <p className="footer-copyright">
-                Copyright 2024 Tomato.com - All Right Reserved.        
+                Copyright 2024 TastyTrails.com - All Right Reserved.        
             </p>
 
         </div>
