20debc172fbccf8ca30185d7e56c14a1dbe683ba
Menath Nadungoda <nadungmenath1@gmail.com>
2024-07-15 10:29:05 +0530

    modified with new contents

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
-import AppDownload from '../../components/App Download/App Download'
+import AppDownload from '../../components/AppDownload/AppDownload'
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
 
 const Home = () => {
